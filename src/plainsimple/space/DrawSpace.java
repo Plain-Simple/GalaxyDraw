6ad165ec5bc9de1cb17bdd87ee1bd27ad059cf98
stefan4472 <stefan.kussmaul@gmail.com>
2016-02-16 17:41:51 -0500

    Simple Generation Working

--- src/plainsimple/space/DrawSpace.java
+++ src/plainsimple/space/DrawSpace.java
@@ -1,8 +1,39 @@
 package plainsimple.space;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.Random;
+
 /**
  * Draws space.
  */
 public class DrawSpace {
-    
+
+    // color of stars
+    private static Color starColor = Color.WHITE;
+
+    // color of background
+    private static Color backgroundColor = Color.BLACK;
+
+    // used for random number generation
+    private static Random random = new Random();
+
+    public static BufferedImage drawSpace(int imgWidth, int imgHeight, double density,
+                                          int brightness, int size) {
+        BufferedImage generated = new BufferedImage(imgWidth, imgHeight, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D graphics = generated.createGraphics();
+        graphics.setColor(backgroundColor);
+        graphics.fillRect(0, 0, imgWidth, imgHeight);
+        int num_stars = imgWidth * imgHeight / 50 / 50;
+        for (int i = 0; i < num_stars; i++) {
+            drawStar(graphics, random.nextInt(imgWidth), random.nextInt(imgHeight), brightness, size);
+        }
+        return generated;
+    }
+
+    private static void drawStar(Graphics2D graphics, int x, int y, int brightness, int size) {
+        graphics.setColor(starColor);
+        graphics.setColor(new Color(starColor.getRed(), starColor.getBlue(), starColor.getGreen(), brightness));
+        graphics.fillOval(x, y, size, size);
+    }
 }
--- src/plainsimple/space/Main.java
+++ src/plainsimple/space/Main.java
@@ -1,11 +1,24 @@
 package plainsimple.space;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Test Class
  */
 public class Main {
 
     public static void main(String[] args) {
-
+        BufferedImage test_image = DrawSpace.drawSpace(1280, 800, 5, 255, 5);
+        try {
+            File saved = new File("SpaceTest.png");
+            ImageIO.write(test_image, "png", saved);
+            Desktop.getDesktop().open(saved);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
